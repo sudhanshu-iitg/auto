692614280ca011709b3b55792e1b7d64756b4f0d
Sudhanshu Jaisinghani <39648627+sudhanshu-iitg@users.noreply.github.com>
2024-05-25 12:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def webhook():
             return jsonify({"message": "Webhook received!", "data": data}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host='0.0.0.0', port=port)
